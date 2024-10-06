3a50c8b223554b7fc8b86471ea172ccc8b96706f
aditya3141 <118032727+aditya3141@users.noreply.github.com>
2024-10-06 21:20:05 +0530

    change
    change

--- src/Pages/auth/Login.js
+++ src/Pages/auth/Login.js
@@ -45,7 +45,7 @@ const Login = () => {
 
   const loginwithgoogle = () => {
     window.open(
-      "https://aapla-market-backend.onrender.com/api/v1/auth/google/callback",
+      "https://aapla-market-backend.onrender.com/auth/google/callback",
       "_self"
     );
   };
